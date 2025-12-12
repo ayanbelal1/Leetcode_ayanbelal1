ff26deb962fe5a7dc3f0f637d4cb8ae0fc0a5b9b
Ayan Belal <ayanbelal777@gmail.com>
2025-12-12 22:36:51 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+       int left =0;
+       int right = arr.length-1; 
+       while(left<right){
+        int sum= num[left]+num[right];
+        if (sum==target){
+            return (left+right)/2;
+
+        }
+        else if( )
+       } 
+    }
+}
