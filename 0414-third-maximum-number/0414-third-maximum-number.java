06e347e2a823ead515b41fe5c5d7cdddce0896d8
Ayan Belal <ayanbelal777@gmail.com>
2026-02-17 15:51:24 +0530

    Time: 3 ms (76.45%), Space: 44.6 MB (90.64%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.java
+++ 0414-third-maximum-number/0414-third-maximum-number.java
@@ -1,31 +1,31 @@
 class Solution {
     public int thirdMax(int[] nums) {
-        int v1=0;
-        int v2=0;
-        int v3=0;
-        for (int num:nums){
-            int n=num;
-            if ((v1!=0 && n==v1) || (v2!=0 && n==v2) || (v3!=0 && n==v3 )){
+
+        Integer v1 = null;
+        Integer v2 = null;
+        Integer v3 = null;
+
+        for (int n : nums) {
+
+            if ((v1 != null && n == v1) ||
+                (v2 != null && n == v2) ||
+                (v3 != null && n == v3)) {
                 continue;
             }
-            if (v1==0 || n>v1){
-                v3=v2;
-                v2=v1;
-                v1=n;
+            if (v1 == null || n > v1) {
+                v3 = v2;
+                v2 = v1;
+                v1 = n;
             }
-            else if (v2==0 || n>v2){
-                
-                v3=v2;
-                v2=n;
+            else if (v2 == null || n > v2) {
+                v3 = v2;
+                v2 = n;
             }
-            else if (v3==0 || n>v3){
-                v3=n;
+            else if (v3 == null || n > v3) {
+                v3 = n;
             }
         }
 
-        if(v3==0){
-            return v1;
-        }
-        return v3;
+        return (v3 == null) ? v1 : v3;
     }
 }
