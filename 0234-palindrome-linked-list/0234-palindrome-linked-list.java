0561086542f0cff1331d57499a77c7de0d027cf2
Ayan Belal <ayanbelal777@gmail.com>
2026-04-28 22:00:19 +0530

    Time: 6 ms (38.97%), Space: 101 MB (15.26%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -1,60 +1,36 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        ListNode temp=head;
-        int n=1;
-        ArrayList <Integer> arr=new ArrayList<>();
-        boolean b=false;
-        for (int i=1;temp.next!=null;i++){
-            temp=temp.next;
+        ListNode temp = head;
+        int n = 1;
+        ArrayList<Integer> arr = new ArrayList<>();
+        
+        // Count length
+        while (temp.next != null) {
+            temp = temp.next;
             n++;
         }
-        temp=head;
-        if (n%2==0){
-            for(int j=1;j<=n/2;j++){
-                arr.add(temp.val);
-                temp=temp.next;
-
-            }
-
-            for(int k=1;k<=arr.size();k++){
-                if(arr[0]=temp.val){
-                    temp=temp.next;
-                    b=true;
-                }
-                else{
-                    b=false;
-                }
-            }
-        }
-        else{
-            for(int j=1;j<=n/2;j++){
-                arr.add(temp.val);
-                temp=temp.next;
-
-            }
-
-            for(int k=1;k<=arr.size();k++){
-                if(arr[0]=temp.next.val){
-                    temp=temp.next;
-                    b=true;
-                }
-                else{
-                    b=false;
-                }
-            }
+        
+        temp = head;
+        
+        // Store first half
+        for (int j = 1; j <= n / 2; j++) {
+            arr.add(temp.val);
+            temp = temp.next;
         }
         
+        // Skip middle element if odd
+        if (n % 2 != 0) {
+            temp = temp.next;
+        }
         
-        return b;
+        // Compare with second half (reverse order using index)
+        for (int k = arr.size() - 1; k >= 0; k--) {
+            if (arr.get(k) != temp.val) {
+                return false;
+            }
+            temp = temp.next;
+        }
+        
+        return true;
     }
 }
